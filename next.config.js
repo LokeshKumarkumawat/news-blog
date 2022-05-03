0fc08245d1b74f677ff9e7572d7bd6af00fba959
lokeshKumar <lokeshkumargithub@gmail.com>
2022-05-03 13:36:32 +0530

    SHORT  PATH ADD

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // "compilerOptions": {
+  //   "baseUrl":".",
+  //   "paths":{
+  //     "@/components/*":["components/*"],
+  //     "@/styles/*":["styles/*"]
+  //   }
+  // }  
+
+  "compilerOptions": {
+    "baseUrl": ".",
+    "paths": {
+      "@/components/*": ["components/*"]
+    }
+  }
 }
 
 module.exports = nextConfig
+
+
+
